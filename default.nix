b6ff38ab97a0fa74e43181d35f690f5f16393252
Tristan Barry <tristan@tryshift.com>
2022-11-07 08:44:40 -0800

    Update to add some tooling

--- default.nix
+++ default.nix
@@ -5,7 +5,11 @@ stdenv.mkDerivation {
     buildInputs = [
         nodePackages.npm
         nodejs-16_x
+        python310
+        tmux
+        awscli2
     ];
+
     shellHook = ''
         export SHIFT_EXECUTABLE=/Users/tristan/workspace/dl-shift-release/Shift.app/Contents/MacOS/Shift
     '';
